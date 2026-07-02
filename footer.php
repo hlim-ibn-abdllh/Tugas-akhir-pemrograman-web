f90e6032647521b21ea5d620510ba30b16367332
hlim-ibn-abdllh <kikybagaskara@gmail.com>
2026-07-02 21:08:56 +0700

    Add files via upload
    Pembuatan modular view komponen bawah website pada views/layouts/footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div> <!-- End of Container -->
+
+<footer class="bg-brand text-white mt-auto py-3">
+  <div class="container text-center">
+    <small>&copy; <?= date('Y'); ?> Madura Disaster Early Warning System (MDEWS). All Rights Reserved.</small>
+  </div>
+</footer>
+
+<!-- Custom JS -->
+<script src="<?= BASE_URL; ?>/assets/js/main.js"></script>
+
+</body>
+</html>
